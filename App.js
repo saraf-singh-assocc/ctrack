f04b9eaa4d8e0c06efcd123ee3ac4972056d4411
alpha-shrehan <shrehanofficial@gmail.com>
2020-06-22 09:30:25 +0530

    new update

--- App.js
+++ App.js
@@ -11,34 +11,25 @@ app.set('view engine', 'hbs');
 app.set('views',path.join(__dirname + '/src/'))
 
 app.get('/',(req,res)=>{
-    res.render('index/index',{none:null})
+  res.render('index/index',{none:null})
 })
-
 app.get('/about_us',(req,res)=>{
   res.render('about_us/about-us'),{none:null}
 })
-
 app.get('/community_page',(req,res)=>{
   res.render('community_page/community-page',{none:null})
 })
-
 app.get('/images/:loc/:fname',(req,res)=>{
   res.sendFile(path.join(__dirname+'/src/'+req.params.loc+'/'+req.params.fname))
 })
-app.get('/index-homepage', (request, response) =>{
-  response.render('/index-homepage/gmaps',{none:null})
+app.get('/index-homepage', (request, res) =>{
+  res.render('index-homepage/gmaps',{none:null})
 })
-app.get('about_us', (request, response) =>{
-  response.render('about_us/about-us',{none:null})
+app.get('/contact_us', (request, res) =>{
+  res.render('contact_us/contact-us',{none:null})
 })
-app.get('/community_page', (request, response) =>{
-  response.render('/community_page/community-page',{none:null})
-})
-app.get('/contact_us', (request, response) =>{
-  response.render('/contact_us/contact-us',{none:null})
-})
-app.get('/login', (request, response) =>{
-  response.render('/login/login',{none:null})
+app.get('/login', (request, res) =>{
+  res.render('login/login',{none:null})
 })
 console.log("> App is running on http://localhost:8000/");
 app.listen(8000)
